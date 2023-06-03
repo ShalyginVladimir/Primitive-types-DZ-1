a340a5b33750105f80eb3864524b46f624877a6e
Vladimir Shalygin <vova.shalygin.88@mail.ru>
2023-06-03 13:27:01 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int check = 12_542;
+        int bonus = 20;
+        int mili = check / bonus;
+
+        System.out.println("Начисленные мили:" + mili);
+    }
+}
